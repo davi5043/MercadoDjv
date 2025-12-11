bf4952f2972ad1ca4abc3a13f1633ab1ea78b1cf
vitor-henrique-f <vitorcamposfaz@gmail.com>
2025-12-10 21:09:47 -0300

    Ajustes 10/12/2025 ás 21:10

--- js/receber.js
+++ js/receber.js
@@ -2,20 +2,20 @@ import { db , getDocs, collection, updateDoc, deleteDoc, doc } from "./firebase.
 
 
 window.pagarParcial = async function (id, valorAtual) {
-    let pago = Number(prompt("Valor pago:").replace(",", "."))
-    if (pago <= 0) return
+      let pago = Number(prompt("Valor pago:").replace(",", "."))
+      if (pago <= 0) return
 
-    let novoValor = Math.round( (valorAtual - pago) * 100) / 100
+      let novoValor = Math.round( (valorAtual - pago) * 100) / 100  
 
-    if (novoValor <= 0) {
-        await deleteDoc(doc(db, "contas_receber", id))
-        alert("Conta quitada!")
-    } else {
-        await updateDoc(doc(db, "contas_receber", id), { valor: novoValor })
-    }
+     if (novoValor <= 0) {
+          await deleteDoc(doc(db, "contas_receber", id))
+          alert("Conta quitada!")
+     } else {
+         await updateDoc(doc(db, "contas_receber", id), { valor: novoValor })
+     }
 
-    atualizarReceber()
-}
+     atualizarReceber()
+ }
 
 async function atualizarReceber() {
     const lista = document.getElementById("listaReceber")
@@ -39,7 +39,7 @@ async function atualizarReceber() {
             Original: R$ ${d.valor.toFixed(2)}<br>
             Dias atraso: ${diasAtraso}<br>
             Total: R$ ${total.toFixed(2)}<br>
-            <button onclick="pagarParcial('${item.id}', ${d.valor})">Pagamento</button>
+            <button onclick="pagarParcial('${item.id}', ${total})">Pagamento</button>
         `
         lista.appendChild(li)
     })
